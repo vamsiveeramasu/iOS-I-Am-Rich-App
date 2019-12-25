26cdac3d389a03869b91f38e23e294b544a79d47
vamsiveeramasu <vamsiveeramasu@gmail.com>
2019-12-25 15:50:01 -0800

    Full app

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Vamsi Veeramasu on 8/25/19.
+//  Copyright © 2019 Vamsi Veeramasu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
